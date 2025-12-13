13a79b6c1194f501dbb16e851ada0400ec41ef0d
AlbertoMendez <alberto.mennun@educa.jcyl.es>
2025-12-13 19:03:46 +0100

    Se añade el proyecto casi terminado.
    Se añade el proyecto login logoff tema5 casi terminado, falta construir el array de usuario de forma correcta, añadiendo la fecha de ultimo acceso.

--- AMNDWESProyectoLoginLogoff/config/confDBPDO.php
+++ AMNDWESProyectoLoginLogoff/config/confDBPDO.php
@@ -1,18 +1,18 @@
 <?php
 /*
                 LOCAL
- */
 
-                const RUTA = 'mysql:host=daw207.local.ieslossauces.es;dbname=DBAMNDWESProyectoTema5';
-                const USUARIO = 'userAMNDWESProyectoTema5';
+
+                const RUTA = 'mysql:host=daw207.local.ieslossauces.es;dbname=DBAMNDWESProyectoLoginLogoff';
+                const USUARIO = 'userAMNDWESProyectoLoginLogoff';
                 const PASS = 'paso';
-
+*/
            /*
             * EXPLOTACIÓN
             */
-            /*
-              const RUTA = 'mysql:host=localhost;dbname=DBAMNDWESProyectoTema5';
-              const USUARIO = 'userAMNDWESProyectoTema5';
+
+              const RUTA = 'mysql:host=localhost;dbname=DBAMNDWESProyectoLoginLogoff';
+              const USUARIO = 'userDBAMNDWESProyectoLoginLogoff';
               const PASS = 'CD97ertvct$E';
- */
+
 ?>
